0db7c15323b866f5f4cdb10188af79486dee0ff9
Nedeljko Brakus <mrnejda@gmail.com>
2019-09-30 19:01:41 +0200

    Kreirane klase za zadatke closes #3

---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,27 @@
+package zadaci;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Zadatak4BrisanjeVrednosti {
+    public static void main(String[] args) {
+        Connection c = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:avionRobaSQLite.db");
+
+
+        } catch (Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }finally {
+            try {
+                c.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
